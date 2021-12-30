d193825b3275f209d366626eda821acf3c417286
Atharva Patil <atharva.patil@berkeley.edu>
2021-12-30 20:10:00 +0900

    file organizing

---
+++ leetcode/get_leetcode.js
@@ -0,0 +1,72 @@
+var data;
+const createSection = (topics) => {
+	var section = $('<div/>',{
+						'class': 'list-group',
+					});
+
+	topics.subtopics.forEach(function(subtopic) {
+		var subsection = $('<div/>',{
+							'class': 'list-group-item  flex-column align-items-start subsection',
+						});
+
+		subsection.append(`<div><h6 class="mb-2 mt-1 subtopic-title">${subtopic.title}</h6></div>`);
+		
+		var problems = $('<div/>');
+		var list = $('<ul/>',{
+							'class': 'list-group list-group-flush',
+						});
+		
+		subtopic.problems.forEach(function(problem) {
+			color = "#B2DFDB";
+			if (problem.difficulty == "Medium") {
+				color = "#FFE0B2";
+			} else if (problem.difficulty == "Hard") {
+				color = "#FFCDD2";
+			}
+			
+			var wrapper = $('<a/>', {
+				css: {
+					'height': '100%',
+					'width': '100%'
+				},
+				'class': 'd-flex justify-content-between align-items-center wrapper'
+			});
+			wrapper.attr("href", problem.url);
+			wrapper.attr("target", "_blank");
+
+			var item = $('<li/>',{
+							'class': 'list-group-item',
+							css: {
+								'background-color': color
+							}
+						});
+			
+			item.append(wrapper);
+			wrapper.append(`<div style="color: #454360" class="problem-name">${problem.title}</div>`);
+			
+			if (problem.difficulty == "Easy") {
+				wrapper.append(`<span class="badge badge-success badge-pill">Easy</span>`);
+			} else if (problem.difficulty == "Medium") {
+				wrapper.append(`<span class="badge badge-warning badge-pill">Medium</span>`);
+			} else {
+				wrapper.append(`<span class="badge badge-danger badge-pill">Hard</span>`);
+			}
+			
+			list.append(item);
+		});
+		problems.append(list);
+		subsection.append(problems);
+		section.append(subsection);
+	});
+	console.log(section);
+	return section;
+}
+
+$.getJSON('problems.json', function(data) {
+	console.log(data);
+	data.forEach(function(topic) {
+		$("#parent").append(`<h3>${topic.topic}</h3>`)
+		$("#parent").append(createSection(topic));
+		console.log('added');
+	});
+});
